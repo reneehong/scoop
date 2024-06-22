636c0141d9f192aaadacea67faf12b8a12a224a9
Ryan <ryan.wu365@gmail.com>
2024-06-22 19:21:16 -0400

    my listings title fix

--- client/src/components/pages/MyListings.js
+++ client/src/components/pages/MyListings.js
@@ -6,8 +6,10 @@ import pear_picture from "../../assets/pear_picture.png";
 const tempProduct = {name:"Placeholder", price:"12", description:"This is a placeholder", imageBuffer: pear_picture};
 const MyListings = () => {
   return (
-    <div>
-      <div className="listing-header">my listings</div>
+    <div className="profile-container"> 
+      <div className="profile-header">
+          my listings
+      </div>
       <div className="my-listings">
         {Array.from({ length: 5 }).map((_, index) => (
           <ProductCard product={tempProduct} key={index} isDeletable={true} />
