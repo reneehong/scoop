744fef93a67e8cb8aa3e2378dbc409aed77a82c1
Joel <joel13samuel@gmail.com>
2024-06-22 18:14:04 -0400

    Updated contact us

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Footer.css";
 
-const Footer = () => {
+const Footer = ({ handleContactUsClick }) => {
   return (
     <footer className="footer">
       <div className="footer-content">
@@ -16,7 +16,7 @@ const Footer = () => {
             <Link to="/" className="footer-link">
               about scoop
             </Link>
-            <Link to="/privacypolicy" className="footer-link">
+            <Link to="/privacy-policy" className="footer-link">
               privacy policy
             </Link>
             <Link to="/terms-of-use" className="footer-link">
@@ -24,7 +24,14 @@ const Footer = () => {
             </Link>
           </div>
           <div className="footer-link-container-2">
-            <Link to="/contact-us" className="footer-link">
+            <Link
+              to="#"
+              className="footer-link"
+              onClick={(e) => {
+                e.preventDefault();
+                handleContactUsClick();
+              }}
+            >
               contact us
             </Link>
             <Link to="/accessibility-statement" className="footer-link">
