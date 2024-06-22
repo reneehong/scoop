aee33072f656d3b0fafc182a24b3262ce6e51b27
Ryan <ryan.wu365@gmail.com>
2024-06-22 17:54:44 -0400

    my listings page fix

--- client/src/components/pages/MyListings.js
+++ client/src/components/pages/MyListings.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./MyListings.css";
 import ProductCard from "./ProductCard";
+import pear_picture from "../../assets/pear_picture.png";
 
+const tempProduct = {name:"Placeholder", price:"12", description:"This is a placeholder", imageBuffer: pear_picture};
 const MyListings = () => {
   return (
     <div>
       <div className="listing-header">my listings</div>
       <div className="my-listings">
         {Array.from({ length: 5 }).map((_, index) => (
-          <ProductCard key={index} isDeletable={true} />
+          <ProductCard product={tempProduct} key={index} isDeletable={true} />
         ))}
       </div>
     </div>
