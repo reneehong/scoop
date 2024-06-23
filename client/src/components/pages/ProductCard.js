80c1382d71fd5b5fa97057a42fa1870448f500ad
kathleen <kathleennz11@gmail.com>
2024-06-23 02:31:00 -0400

    typo

--- client/src/components/pages/ProductCard.js
+++ client/src/components/pages/ProductCard.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
-import React, { useState, useEffect } from "react";
 import "./ProductCard.css";
-import axios from "axios";
 import axios from "axios";
 import DeleteButton from "./DeleteButton.js";
 import { Link } from "react-router-dom";
