ee04a6cac91dd595d2abbdd4b3dce4ea90c3ae29
Ryan <ryan.wu365@gmail.com>
2024-06-22 21:41:00 -0400

    scuffed but semi-working delete button

--- client/src/components/pages/MyListings.js
+++ client/src/components/pages/MyListings.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import "./MyListings.css";
 import ProductCard from "./ProductCard";
 import pear_picture from "../../assets/pear_picture.png";
 
 const tempProduct = {name:"Placeholder", price:"12", description:"This is a placeholder", imageBuffer: pear_picture};
 const MyListings = () => {
+  const [isComponentVisible, setIsComponentVisible] = useState(true);
+  const handleDelete = () => {
+    setIsComponentVisible(false);
+  };
   return (
     <div className="profile-container"> 
       <div className="profile-header">
           my listings
       </div>
       <div className="my-listings">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <ProductCard product={tempProduct} key={index} isDeletable={true} />
+        {Array.from({ length: 1 }).map((_, index) => (
+          isComponentVisible && <ProductCard product={tempProduct} key={index} isDeletable={true} onDelete={handleDelete}/>
         ))}
       </div>
     </div>
